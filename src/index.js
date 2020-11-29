d89af4b92a40613bbd298d2da1814d7f34c0032e
Dhony <dhony.web@gmail.com>
2020-11-29 16:27:17 -0300

    Components and Props.

--- src/index.js
+++ src/index.js
@@ -140,43 +140,75 @@ function Square(props) {
   }
   
   // ========================================
-  
-function formatName(user){
-  return user.firstName + ' ' + user.lastName;
-}
 
-function getGreeting(user) {
-  if (user) {
-    return <h1>Olá {formatName(user)}!</h1>;
+  function formatDate(date) {
+    return date.toLocaleDateString();
   }
-  return <h1>Olá Estranho.</h1>;
-}
 
-const user = {
-  firstName: 'Dhony',
-  lastName: 'Silva'
-};
-  
-  const element = React.createElement (
-    'h1',
-    {className: 'greeting'},
-    'hello world!'
-  );
+  function Welcome(props) {
+    return <h1>Olá { props.name}</h1>;
+  }
 
-  function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+  function Avatar(props) {
+    return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+    );
+  }
+
+  function UserInfo(props) {
+    return (
+      <div className="UserInfo">
+          <Avatar user={props.user} />
+          <div className="UserInfo-name">
+            {props.user.name}
+          </div>
       </div>
     );
-    ReactDOM.render(element,
-      document.getElementById('root'));
   }
-  setInterval(tick, 1000);
+
+  function Comment(props) {
+    return (
+      <div className="Comment">
+        <UserInfo user={props.author} />
+          <div className="Comment-text">
+            {props.text}
+          </div>
+          <div className="Comment-date">
+            {formatDate(props.date)}
+          </div>
+        </div>
+    );
+  }
+
+  const comment = {
+    text: 'Aprender React é um habilidade muito importante!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+  };
+
+  function App() {
+    return (
+      <div>
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edit" />
+        <Comment
+          date={comment.date}
+          text={comment.text}
+          author={comment.author}
+        />
+      </div>
+    );
+  }
+  
 
   ReactDOM.render(
-    element,
+    <App />,
     
     /* <Game />, */
     
