eecf9f75c4e8f6fadbc9b02997ab9aeb2813c79d
Dhony <dhony.web@gmail.com>
2020-11-30 22:12:20 -0300

    Conditional Rendering.

--- src/index.js
+++ src/index.js
@@ -315,6 +315,42 @@ function Square(props) {
 
   const messages = ['React', 'Re: React', 'Re:Re:React'];
 
+  function WarningBanner(props) {
+    if(!props.warn) {
+      return null;
+    }
+    return (
+      <div className="warning">
+        Warning!
+      </div>
+    );
+  }
+
+  class Page extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {showWarning: true};
+      this.handleToggleClick = this.handleToggleClick.bind(this);
+    }
+
+    handleToggleClick() {
+      this.setState(state => ({
+        showWarning: !state.showWarning
+      }));
+    }
+
+    render() {
+      return (
+        <div>
+          <WarningBanner warn={this.state.showWarning} />
+          <button onClick={this.handleToggleClick}>
+            {this.state.showWarning ? 'Hide' : 'Show'}
+          </button>
+        </div>
+      );
+    }
+  }
+
   function App() {
     return (
       <div>
@@ -322,6 +358,7 @@ function Square(props) {
         <Clock />
         <LoginControl />
         <Mailbox unreadMessages={messages} />
+        <Page />
       </div>
     );
   }
