df26a35e929cc9dc84e462c0f3f657759173e0fe
Dhony <dhony.web@gmail.com>
2020-11-30 21:52:58 -0300

    Conditional Rendering - Login Control.

--- src/index.js
+++ src/index.js
@@ -226,14 +226,85 @@ function Square(props) {
       }
   }
 
+  class LoggingButton extends React.Component {
+    handleClick() {
+      console.log('this is:', this);
+    }
+
+    render() {
+      return (
+        <button onClick={() => this.handleClick()}>
+          Click me
+        </button>
+      );
+    }
+  }
+
+  function UserGreeting(props) {
+    return <h1>Bem vindo de volta!</h1>;
+  }
+  function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+  }
+  function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+      return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+  }
+
+  function LoginButton(props) {
+    return (
+      <button onClick={props.onClick}>Login</button>
+    );
+  }
+  function LogoutButton(props) {
+    return (
+      <button onClick={props.onClick}>Logout</button>
+    );
+  }
   
+
+  // Statefull Component
+  class LoginControl extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleLoginClick = this.handleLoginClick.bind(this);
+      this.handleLogoutClick = this.handleLogoutClick.bind(this);
+      this.state = {isLoggedIn: false};
+    }
+
+    handleLoginClick() {
+      this.setState({isLoggedIn: true});
+    }
+    handleLogoutClick() {
+      this.setState({isLoggedIn: false});
+    }
+    
+    render() {
+      const isLoggedIn = this.state.isLoggedIn;
+      let button;
+      if (isLoggedIn) {
+        button = <LogoutButton onClick={this.handleLogoutClick} />;
+      } else {
+        button = <LoginButton onClick={this.handleLoginClick} />;
+      }
+      return (
+        <div>
+          <Greeting isLoggedIn={isLoggedIn} />
+          {button}
+        </div>
+      );
+    }
+  }
+
   function App() {
     return (
       <div>
         <Welcome name="Dhony" />
         <Clock />
-        <Clock />
-        <Clock />
+        <LoginControl />
       </div>
     );
   }
