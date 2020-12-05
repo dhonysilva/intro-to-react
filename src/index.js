7a164ef65b88156b1d856c479e57b8db03407240
Dhony <dhony.web@gmail.com>
2020-12-05 13:16:21 -0300

    Forms v2

--- src/index.js
+++ src/index.js
@@ -402,7 +402,7 @@ function Square(props) {
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: 'Please write an essay about your favorite DOM element.'};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -421,8 +421,8 @@ class NameForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          Essay:
+          <textarea value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
